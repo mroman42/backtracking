4492b4f531c926cd5a706fcddc2f2b16f5f0812f
M42 <mromang08@gmail.com>
2014-04-27 14:07:19 +0200

    Mochila en un sólo main. Prefiero escribirlo así antes que liar un montón de funciones llamándose y pasándose miles de argumentos.

--- src/mochila.cpp
+++ src/mochila.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include <queue>
 using namespace std;
+typedef unsigned int uint;
 
 /**
  * Formato del problema: 
@@ -12,45 +13,64 @@ using namespace std;
  */
 
 
-
-class Objeto {
-public:
-    Objeto (int peso_init, int valor_init)
-	:peso(peso_init), valor(valor_init)
-    {}
-
-    const int peso;
-    const int valor;
-};
-
-typedef vector<bool> Mochila;
-Mochila rellenar (Mochila& mochila, int n, vector<Objeto>& objeto, int tamanio) {
-}
-
 int main () {
     // Lectura del problema
-    int tamanio;
-    int numero;
+    int limite;
+    uint tamanio;
+    cin >> limite;
+    cin >> tamanio;
+    
     vector<int> pesos;
     vector<int> beneficios;
-    cin >> tamanio;
-    cin >> numero;
-    
     int leido;
-    for (int i=0; i<numero; i++) {
+    for (uint i=0; i<tamanio; i++) {
 	cin >> leido;
 	pesos.push_back(leido);
     }
-    for (int i=0; i<numero; i++) {
+    for (uint i=0; i<tamanio; i++) {
 	cin >> leido;
 	beneficios.push_back(leido);
     }
 
-    vector<Objeto> objetos;
-    for (int i=0; i<numero; i++)
-	objetos.push_back(Objeto(pesos[i], beneficios[i]));
-
-
     // Resolución del problema
-    Mochila mochila;
+    typedef vector<bool> Mochila;
+    Mochila solucion;
+    int max_valor = 0;
+    queue<Mochila> posibles_mochilas;
+    
+    posibles_mochilas.push(Mochila());
+    while (!posibles_mochilas.empty()) {
+	Mochila actual = posibles_mochilas.front();
+	posibles_mochilas.pop();
+	
+	// Caso de mochila llena
+	// Calculamos su valor y si es mejor que la mejor mochila actual.
+	if (actual.size() == tamanio) {
+	    int valor = 0;
+	    for (uint i=0; i<tamanio; i++)
+		valor += actual[i]? beneficios[i] : 0;
+
+	    if (valor > max_valor) {
+		max_valor = valor;
+		solucion = actual;
+	    }
+	}
+
+	// Caso de la mochila sin llenar
+	// Rellenamos o no con el siguiente objeto.
+	// Añadimos el nuevo si no excede el peso.
+	else {
+	    Mochila con_nuevo = actual;
+	    Mochila sin_nuevo = actual;
+	    con_nuevo.push_back(true);
+	    sin_nuevo.push_back(false);
+
+	    int nuevo_peso = 0;
+	    for (uint i=0; i<tamanio; i++)
+		nuevo_peso += actual[i]? pesos[i] : 0;
+	    if (nuevo_peso <= limite)
+		posibles_mochilas.push(con_nuevo);
+	    posibles_mochilas.push(sin_nuevo);
+	}
+    }
 }

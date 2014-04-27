403f62a1c3600267905b283368b3e73f3a39764b
M42 <mromang08@gmail.com>
2014-04-27 13:39:36 +0200

    Cambiando funciones en Haskell. Si se pudiera aprovechar todo quedaría bastante más limpio que en C++, pero no sé cómo hacerlo.

--- src/backtracking.hs
+++ src/backtracking.hs
@@ -1,15 +1,25 @@
-{- Función de Backtracking -}
+-- Función de Backtracking
+{- Genera todas las posibles soluciones a un problema de backtracking.
+   Pide una función que devuelve los nodos hijos de una pseudosolución
+   y una función de poda que devuelva verdadero o falso sobre cada
+   pseudosolución.
+
+   Las soluciones sin pseudosoluciones hijas se consideran soluciones finales.
+ -}
 backtracking :: (a -> [a]) -> (a -> Bool) -> a -> [a]
 backtracking nodes pred x 
-    | (pred x && nodes x == []) = x  
-    | pred x = concat map backtracking (nodes x)
+    | (pred x && (length (nodes x) == 0)) = [x]
+    | pred x = concat $ map (backtracking nodes pred) (nodes x)
     | otherwise = []
                   
+                  
 {- Mochila -}
-type Mochila = [Int]
-
-mochila :: [Int] -> [Int] -> Mochila -> [Mochila]
-mochila weights benefits x = backtracking push full x
+type Mochila = ([Int], Int)
+mochilaBT :: [Int] -> [Int] -> Int -> Mochila -> [Mochila]
+mochilaBT weights benefits limit x = backtracking push isFull x
           where
-            push x = 
-            
+            totalWeight x = foldr (+) 0 (map (weights !!) x)
+            isFull (xs,k) = ((totalWeight xs) > limit)
+            push (xs,k)
+              | k < (length benefits) = [((xs:k),k+1), (xs,k+1)]
+              | otherwise = []

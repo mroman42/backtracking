537edf37744b15068850380da96077520109eab9
M42 <mromang08@gmail.com>
2014-04-28 17:44:32 +0200

    Backtracking del TSP. **Sin comprobar. Falta compilarlo.** Implementa la fuerza bruta sobre el TSP. Puede optimizarse el cálculo de las distancias para evitar repetir cálculos sobre la distancia entre ciudades iguales.

--- src/tsp.cpp
+++ src/tsp.cpp
@@ -7,13 +7,60 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cmath>
+#include <limits>
+
 using namespace std;
 typedef unsigned int uint;
 typedef pair<int,int> Point;
+typedef vector<int> Ruta;
+typedef float Coste;
+
+
 vector<Point> ciudades;
 int dimension;
+Ruta mejor_ruta;
+Coste mejor_coste;
 
-void permutaciones () {
+
+Coste distancia (Point a, Point b) {
+    // Calcula la distancia entre dos puntos.
+    Coste x = a.first - b.first;
+    Coste y = a.second - b.second;
+    return sqrt(x*x - y*y);
+}
+
+
+void permutaciones (Ruta& ruta, Coste& coste_actual, int indice) {
+    // Caso de la ruta finalizada
+    // Comprueba si se mejora el óptimo.
+    if (indice == dimension) {
+	if (coste_actual < mejor_coste) {
+	    mejor_ruta = ruta;
+	    coste_acutal = mejor_coste;
+	}
+    }
+
+    // Caso de recorrido intermedio
+    // Prueba posibles permutaciones para los restantes elementos.
+    else {
+	for (int i=indice; i<dimension; i++) {
+	    // Produce una permutación en la ruta.
+	    Point temp = ruta[i];
+	    ruta[i] = ruta[indice];
+	    ruta[indice] = ruta[i];
+	    coste_actual += distancia(ruta[indice-1], ruta[indice]);
+	    
+	    // Estudia permutaciones con ese cambio.
+	    permutaciones (ruta, coste_actual, indice+1);
+
+	    // Deshace la permutación.
+	    coste_actual -= distancia(ruta[indice-1], ruta[indice]);
+	    temp = ruta[i];
+	    ruta[i] = ruta[indice];
+	    ruta[indice] = ruta[i];
+	}
+    }
 }
 
 
@@ -29,12 +76,15 @@ int main() {
 
 
     // Resolución del problema
-    // Para cada ciudad visitada, añade a sus vecinos no visitados a la cola.
-    queue<Point> a_visitar;
-
-    a_visitar.push(ciudades[0]);
-    while (!a_visitar.empty()) {
-	Point actual = a_visitar.front();
-	a_visitar.push();
+    // Recorre las posibles permutaciones dejando fija la primera ciudad.
+    // Crea una primera ruta con la permutación identidad.
+    Ruta ruta;
+    for (int i=0; i<dimension; ++i) {
+	ruta.push_back(i);
     }
+    
+    mejor_ruta = ruta;
+    mejor_coste = numeric_limits<Coste>::infinity();
+
+    permutaciones(ruta, 0, 1);
 }

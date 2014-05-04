2345adc91728537fbe2029fe114462c2d229804c
M42 <mromang08@gmail.com>
2014-05-04 18:31:09 +0200

    using namespace std;

--- src/mochila.cpp
+++ src/mochila.cpp
@@ -13,21 +13,22 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+using namespace std;
 typedef unsigned int uint;
-typedef std::vector<bool> Mochila;
+typedef vector<bool> Mochila;
 
 template<class T>
-std::ostream& operator<< (std::ostream& output, std::vector<T>& v){
+ostream& operator<< (ostream& output, vector<T>& v){
     for (auto i : v)
         output << i << ' ';
     
-    output << std::endl;
+    output << endl;
 }
 
-std::pair<Mochila, int> resolver(int limite, std::vector<int> pesos, std::vector<int> beneficios) {
+pair<Mochila, int> resolver(int limite, vector<int> pesos, vector<int> beneficios) {
     // Resolución del problema
     uint tamanio = pesos.size();
-    std::queue<Mochila> posibles_mochilas;
+    queue<Mochila> posibles_mochilas;
     Mochila solucion;
     int max_valor = 0;
     
@@ -70,37 +71,36 @@ std::pair<Mochila, int> resolver(int limite, std::vector<int> pesos, std::vector
         }
     }
 
-    return std::pair<Mochila, int>(solucion, max_valor);
+    return pair<Mochila, int>(solucion, max_valor);
 } 
 
 int main () {
     // Lectura del problema
     int limite;
     uint tamanio;
-    std::cout << "Introduce límite de peso de la mochila: ";
-    std::cin >> limite;
-    std::cout << "Introduce número de objetos de la mochila: ";
-    std::cin >> tamanio;
+    cout << "Introduce límite de peso de la mochila: ";
+    cin >> limite;
+    cout << "Introduce número de objetos de la mochila: ";
+    cin >> tamanio;
     
-    std::vector<int> pesos;
-    std::vector<int> beneficios;
+    vector<int> pesos;
+    vector<int> beneficios;
     int leido;
-    std::cout << "Introduce pesos, separados por espacios:" << std::endl;
+    cout << "Introduce pesos, separados por espacios:" << endl;
     for (uint i=0; i<tamanio; i++) {
-        std::cin >> leido;
+        cin >> leido;
         pesos.push_back(leido);
     }
-    std::cout << "Introduce beneficios, separados por espacios:" << std::endl;
+    cout << "Introduce beneficios, separados por espacios:" << endl;
     for (uint i=0; i<tamanio; i++) {
-        std::cin >> leido;
+        cin >> leido;
         beneficios.push_back(leido);
     }
 
-    std::pair<Mochila, int> resultado = resolver(limite, pesos, beneficios);
+    pair<Mochila, int> resultado = resolver(limite, pesos, beneficios);
 
     // Muestra la solución.
-    // Muestra la solución.
-    std::cout << std::endl << "SOLUCIÓN:" << std::endl 
-        << "Valor obtenido: " << resultado.second << std::endl
-        << "Mochila: " << resultado.first << std::endl;
+    cout << endl << "SOLUCIÓN:" << endl 
+        << "Valor obtenido: " << resultado.second << endl
+        << "Mochila: " << resultado.first << endl;
 }

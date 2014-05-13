24ae40346013ae04a3cc271a24c56f64daccf00d
David Charte <fdavidcl@outlook.com>
2014-05-13 21:52:44 +0200

    Comenzando planificación
    Añadida función de cálculo de tiempo en procesador, comenzada estructura de la función de resolución.

--- src/planificacion.cpp
+++ src/planificacion.cpp
@@ -1,15 +1,17 @@
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using std::vector;
 
 struct Tarea;
 
-typedef vector<Tarea> procesador;
+typedef double tiempo;
+typedef vector<Tarea>  procesador; // Importantes los dos espacios!
 
 /* Tarea v1: struct */
 struct Tarea {
-    double ejecucion;
+    tiempo ejecucion;
 
     // Como el grafo es acíclico, cada Tarea solo puede depender de una única Tarea.
     Tarea* dependencia;
@@ -63,15 +65,63 @@ struct cmp {
     }
 };
 
-vector<procesador> planifica(vector<Tarea>) {
+// Calcula el tiempo (coste) de una ordenación de tareas en un multiprocesador
+tiempo hallaTiempo(vector<procesador> historial) {
+    tiempo max_time = 0;
+    int num_cores = historial[0].size();
+
+    for (int i = 0; i < num_cores; ++i) {
+        tiempo suma = 0;
+
+        for (auto& m : historial)
+            suma += m[i].ejecucion;
+
+        if (suma > max_time)
+            max_time = suma;
+    }
+
+    return max_time;
+}
+
+vector<procesador> planifica(vector<Tarea>, int num_cores) {
+    std::queue<vector<procesador>> posibles;
+    vector<procesador> solucion;
+
+    posibles.push(vector<procesador>());
+    posibles.front().push_back(procesador(num_cores, (Tarea){0,0} ));
+
+    while (!posibles.empty()) {
+        //vector<procesador> actual = posibles.front();
+        //procesador ahora = actual.front();
+        //posibles.pop();
+
+        bool very_dependencia = false;
+        int c = 0;
+
+        while (!very_dependencia && c < num_cores) {
+            //actual[c] = 0;
+            /* TODO
+                Asignar tareas al procesador actual, evitando asignar
+                tareas dependientes de posteriores o actuales
+
+                Añadir un procesador más con las siguientes tareas de
+                cada core, calculadas de la misma forma
+
+                Contabilizar el coste y encontrar la solución óptima.
+            */
+            c++;
+        }
+
+    }
+
     return vector<procesador>();
 }
 
 int main (int argc, char const *argv[]) {
     vector<Tarea> tareas;
 
-    tareas.push_back((Tarea){2, 0});
-    //tareas.push_back({2, &tareas[0]});
+    tareas.push_back({2, 0});
+    tareas.push_back({2, &tareas[0]});
 
     return 0;
 }

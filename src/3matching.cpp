4d46f701c474edf4b0dc5c8fc48b5f68ffafca84
M42 <mromang08@gmail.com>
2014-05-13 18:40:22 +0200

    Algoritmo del 3d matching. Siguiendo el ejemplo de la mochila.

--- src/3matching.cpp
+++ src/3matching.cpp
@@ -39,7 +39,43 @@ int main () {
 	aristas.push_back(Triple(a,b,c));
 
 
+    // Bloque de cómputos
+    // El tamaño de cada tabla de nodos usados será la mayor de sus componentes.
+    int size1 = 0;
+    int size2 = 0;
+    int size3 = 0;
+    for (Triple arista : aristas) {
+	if (size1 < get<1>(arista))
+	    size1 = get<1>(arista);
+	if (size2 < get<2>(arista))
+	    size2 = get<2>(arista);
+	if (size3 < get<3>(arista))
+	    size3 = get<3>(arista);
+    }
 
+    // Prueba combinaciones de aristas.
+    // Marca como true las aristas escogidas.
+    queue<Matching> posibles_particiones;
+    Matching solucion;
+    int tamanio = aristas.size();
+    
+    while (!posibles_particiones.empty()) {
+	Matching actual = posibles_particiones.top();
+	posibles_particiones.pop();
+
+	// Caso de matching completo
+	if (actual.aristas.size() == tamanio) {
+            if (actual.valor > solucion.valor)
+                solucion = actual;
+        }
+
+	// Caso de matching por completar.
+	// Añade el caso de que se obtega la arista o no.
+	else {
+	    Matching con_nueva = actual;
+	    Matching sin_nueva = actual;
+	}
+    }
     
     // Bloque de salidas
     
